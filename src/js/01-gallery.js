603bb0a91e835d27a825fc0dea2d521e4458fa1b
Romanna Yasinska <r.bren.1991@gmail.com>
2021-12-21 19:41:58 +0200

    Update task 1 add handlebars

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Імпорт розмітки із шаблонізатора
+import GalleryMarkupTml from '../templates/gallery-markup.hbs';
 // Change code below this line
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -8,22 +10,10 @@ console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
 
-// Функція для рендеру розмітки
+// Функція для рендеру розмітки(змінила з використанням функції шаблонізатора)
 
 function createGalleryMarkup(galleryItems) {
-return galleryItems.map(({preview, original, description}) => 
-  { return `
-  <div class="gallery__item">
-    <a class="gallery__item" href="${original}">
-        <img
-            class="gallery__image"
-            src="${preview}"
-            alt="${description}"
-        />
-    </a>
-  </div>
-  `
-  }).join("")
+return GalleryMarkupTml(galleryItems)
 }
 // додаю розмітку в документ
 gallery.insertAdjacentHTML('beforeend', galleryMarkup);
